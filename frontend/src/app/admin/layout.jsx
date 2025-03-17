5a2dda60043f4131849c4065f1749f54be15e5de
Kunal-Sharma816 <22bcs058@iiitdwd.ac.in>
2025-03-17 23:51:51 +0530

    admin updation

--- frontend/src/app/admin/layout.jsx
+++ frontend/src/app/admin/layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
       <AdminSidebar />
       <main className="w-full relative">
         <header className="border-b h-14 flex items-center px-2 sticky top-0 bg-white">
-          <SidebarTrigger className={"hover:bg-zinc-100"} />
+          <SidebarTrigger className={"hover:bg-zinc-200"} />
         </header>
         <div className="flex flex-col gap-6 px-8 pb-6 pt-3">{children}</div>
       </main>
