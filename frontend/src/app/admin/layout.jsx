e4adcb0ed704d9253d848b0e6c675b69e4176e7f
Kunal-Sharma816 <22bcs058@iiitdwd.ac.in>
2025-03-18 12:43:41 +0530

    also do minort updation in UI and in admin

--- frontend/src/app/admin/layout.jsx
+++ frontend/src/app/admin/layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
     <SidebarProvider>
       <AdminSidebar />
       <main className="w-full relative">
-        <header className="border-b h-14 flex items-center px-2 sticky top-0 bg-white">
+        <header className="border-b h-14 flex items-center px-2 sticky top-0 bg-white z-10">
           <SidebarTrigger className={"hover:bg-zinc-200"} />
         </header>
         <div className="flex flex-col gap-6 px-8 pb-6 pt-3">{children}</div>
